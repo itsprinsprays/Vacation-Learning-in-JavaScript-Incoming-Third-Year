a2d633148c853c3215e7b9c8a2d756d77b46f47b
itsprinsprays <prncbntz@gmail.com>
2026-06-26 18:11:32 +0800

    Day 2: Variable with Interpolation Calling

---
+++ Day2/js/script.js
@@ -0,0 +1,6 @@
+let neighbor1 = "Prince"; 
+let neighbor2 = "Jediel";
+let pronoun = "He";
+let noun = "coffee";
+
+document.getElementById("text").innerHTML = `${neighbor1}, the friendly neighbor, waved at ${neighbor2} as ${pronoun} walked by ${neighbor2}'s house. ${neighbor2} smiled back and invited ${neighbor1} in for a cup of ${noun}`;
