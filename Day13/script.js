701a0d352cea73410ee2c2ff013cb537dd1b88bf
itsprinsprays <prncbntz@gmail.com>
2026-06-30 16:12:18 +0800

    Document Object Model

---
+++ Day13/script.js
@@ -0,0 +1,32 @@
+let title = document.querySelector("#first-title");
+title.innerHTML = "Titanic"
+
+let Description = document.querySelectorAll(".desc");
+Description[0].innerHTML = "Romance";
+Description[1].innerHTML = "Love"
+
+//input type
+let firstname = document.querySelector("#firstname")
+firstname.placeholder = "Enter first name";
+
+
+//changing src images
+let images = document.querySelector("#images");
+images.src = "https://tse1.mm.bing.net/th/id/OIP.0JbZkj5mDd4eiI6qAkUebQHaE8?pid=Api&P=0&h=180"
+
+//changing href or url
+let links = document.querySelector("#link");
+links.href = "https://www.youtube.com"
+links.target = "_blank";
+
+
+let body = document.querySelector("body");
+body.style.backgroundColor = "rgb(211, 179, 179)";
+
+let h1 = document.querySelector("h1");
+h1.style.textAlign = "center";
+h1.style.fontFamily = "Montserrat";
+h1.style.color = "white";
+
+
+
