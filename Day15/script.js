dcc10feb34db9b6e0b89a4f7e29682c085d7c7e9
itsprinsprays <prncbntz@gmail.com>
2026-07-02 15:53:06 +0800

    JSON Destructuring, Arrow function, Events, Timing Events and Timing Intervals

---
+++ Day15/script.js
@@ -0,0 +1,52 @@
+function readInput(e) { 
+
+    let value = e.target.value;
+    alert(value);
+
+}
+
+//Arrow Function
+let add = (x,y) => x + y;
+alert(add(5, 3));
+
+
+//timing events
+// setTimeout(() => {
+
+// let input = document.querySelector("input");
+// input.style.transtintion = "1s";
+// input.placeholder = "Enter your name";
+
+// let body = document.querySelector("body");
+// body.style.transition = "1s";
+// body.style.backgroundColor = "red";
+
+// }, 2000);
+
+
+//set Interval
+setInterval(() => {
+    let body = document.querySelector("body");
+    body.style.transition = "1s";
+    if(body.style.backgroundColor === "blue") body.style.backgroundColor = "black";
+    else body.style.backgroundColor = "blue";
+
+
+}, 2000);
+
+
+
+const user = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "email": "john@example.com"
+};
+
+let {name, age, city, email} = user;
+alert(name);
+alert(age);
+alert(city);
+alert(email);
+
+
