66c3cd4bb6e4527dea9d4db253be0265d9c86012
itsprinsprays <prncbntz@gmail.com>
2026-07-03 21:57:32 +0800

    Map

---
+++ Day16/FilterScript.js
@@ -0,0 +1,7 @@
+let numbers = [45, 60, 90, 35, 78, 20];
+
+let evenNumbers = numbers.filter(num => num % 2 === 0);
+let greaterThan50 = numbers.filter(num => num > 50);
+
+alert(evenNumbers);
+alert(greaterThan50);
