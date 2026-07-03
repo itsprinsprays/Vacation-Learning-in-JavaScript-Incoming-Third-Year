29c8b8ebc3c40b6ab3233c72a3af4aed3e0e495b
itsprinsprays <prncbntz@gmail.com>
2026-07-03 22:09:29 +0800

    Filter

--- Day16/FilterScript.js
+++ Day16/FilterScript.js
@@ -5,3 +5,48 @@ let greaterThan50 = numbers.filter(num => num > 50);
 
 alert(evenNumbers);
 alert(greaterThan50);
+
+
+let students = [ 
+    { name: "Prince", score: 95},
+    { name: "John", score: 68},
+    { name: "Maria", score: 88},
+    { name: "James", score: 70}
+];
+
+let passedStudents = students.filter(student => student.score >= 75);  
+console.log(passedStudents);
+
+let product = [
+    { name: "Laptop", inStock: true},
+    { name: "Mouse", inStock: false},
+    { name: "Keyboard", inStock: true},
+    { name: "Monitor", inStock: false}
+];
+
+let stockProduct = product.filter(stock => stock.inStock === true);
+
+console.log(stockProduct);
+
+const names = [
+    "Prince",
+    "John",
+    "Paul",
+    "Maria",
+    "Patrick"
+];
+
+let startsWithP = names.filter(pStart => pStart.startsWith("P"));
+alert(startsWithP);
+
+let people = [
+{ name: "Prince", age: 20 },
+{ name: "John", age: 16 },
+{ name: "Maria", age: 19 },
+{ name: "James", age: 15 },
+{ name: "Anna", age: 25 }
+];
+
+let adult = people.filter(adults => adults.age > 18);
+
+console.log(adult);
