8ebc659a32b30c37f1100205fbd2334edf47cd48
itsprinsprays <prncbntz@gmail.com>
2026-07-19 12:36:34 +0800

    React Structure

---
+++ Day21/CourseManagement/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Course Management System</h1>
+      <p>Welcome to my first React application!</p>
+    </div>
+  )
+}
+
+export default App
