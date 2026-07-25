e312a3beca93fcfbb0afeb2de54085e98a75ff9c
itsprinsprays <prncbntz@gmail.com>
2026-07-25 22:48:00 +0800

    Colors Changing

--- Day21/CourseManagement/src/components/Counter.jsx
+++ Day21/CourseManagement/src/components/Counter.jsx
@@ -14,17 +14,21 @@ export default function Counter() {
         }
     ];
 
+    const colors = ["Red","Blue", "Green", "Yellow", "Purple"
+];
+
     const [count, setCount] = useState(0);
     const [name, setName] = useState("");
     const [age, setAge] = useState(0);
     const [course, setCourse] = useState("");
     const [show, setShow] = useState(false);
+    const [color, setColor] = useState("");
 
     return (
         <>
 
         <h1>{count}</h1>
-        <h1>Name :{name}</h1>  
+        <h1 style={{color}}>Name :{name}</h1>  
         <h1>Age : {age}</h1>    
         <h1>Course : {course}</h1>  
 
@@ -38,6 +42,7 @@ export default function Counter() {
                 setName(students[nextCount - 1].name)
                 setAge(students[nextCount - 1].age)
                 setCourse(students[nextCount - 1].course)
+                setColor(colors[nextCount - 1])
             } else alert("No Student Ahead!");
         }
         }}> Increase</button>
