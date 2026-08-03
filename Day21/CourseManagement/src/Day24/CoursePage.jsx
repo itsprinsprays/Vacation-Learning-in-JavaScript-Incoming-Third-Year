1fa4d7c5d0545781136d003a1cab3903aa1a3cd1
itsprinsprays <prncbntz@gmail.com>
2026-08-03 18:31:26 +0800

    Add input

--- Day21/CourseManagement/src/Day24/CoursePage.jsx
+++ Day21/CourseManagement/src/Day24/CoursePage.jsx
@@ -1,27 +1,39 @@
 import { useState, useEffect} from "react";
 import { getCourseById } from "./CourseService.js";
 import CourseDetails from "./CourseDetails.jsx";
+import CourseInput from "./CourseInput.jsx";
 
 export default function CoursePage() {
     const [course, setCourse] = useState(null);
+    const [courseId, setCourseId] = useState("");
+    const [error, setError] = useState("");
 
-    useEffect(() => {
+
         async function fetchCourseById() {
             try {
 
-                const data = await getCourseById(3);
+                const data = await getCourseById(courseId);
                 setCourse(data);
 
             } catch(error) {
                 console.error("Error fetching course data:", error);
+                setError(error.message);
             }
         }
-        fetchCourseById();
-    }, []);
 
-    if(!course) {
-        return <h1>Loading...</h1>
-    }
 
-    return <CourseDetails course={course} />
+
+    return (
+         <div>
+            <h1>Course Page</h1>
+            <CourseInput
+                courseId={courseId}
+                setCourseId={setCourseId}
+                onFetch={fetchCourseById}
+            />
+
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            {course ? <CourseDetails course={course} /> : <p>No course loaded yet</p>}
+        </div>
+    );
 }
