76a55f5b0f1e63de40ecd96697e126cc4bb36109
itsprinsprays <prncbntz@gmail.com>
2026-08-15 18:59:00 +0800

    Tailwind Added on Day25 Create Course

--- Day21/CourseManagement/src/Day25/CreateCourse.jsx
+++ Day21/CourseManagement/src/Day25/CreateCourse.jsx
@@ -27,7 +27,8 @@ export default function CreateCourse({ onCreate }) {
 
     return (
         <>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="bg-[#F2EEED] w-[400px] flex flex-col justify-center items-center p-6 border rounded m-[10px] shadow-2xl">
+            <h1 className="text-[black] font-bold px-5 self-start">Create Course</h1>
 
             <input
                 type="text"
@@ -35,6 +36,7 @@ export default function CreateCourse({ onCreate }) {
                 value={course.courseName}
                 name="courseName"
                 onChange={handleChange}
+                className="text-center mb-3 border rounded p-2 w-full"   
             />
 
             <input 
@@ -43,9 +45,10 @@ export default function CreateCourse({ onCreate }) {
                 value={course.unit}
                 name="unit"
                 onChange={handleChange}
+                className="text-center mb-3 border rounded p-2 w-full"
             />
 
-        <button type="submit">Submit</button>
+        <button type="submit" className="bg-[#60A5FA] self-end text-right border rounded px-[20px] py-[5px] transition-colors duration-300 hover:text-gray-100 hover:bg-[#1E40AF]">Submit</button>
         
 
         </form>
